cf1583e41ecca5939bd93b085325187dac4bc0ab
genesisbpa10 <genesisbetaly@gmail.com>
2022-10-20 23:16:06 -0500

    crearcuenta y home corregidos

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -8,67 +8,75 @@ import {
   ScrollView,
 } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
-import styles from "../Styles";
+import loginStyles from "../styles/LoginStyles";
+import buttonStyles from "../styles/ButtonStyles";
 import { Entypo } from "@expo/vector-icons";
+import styles from "../Styles";
+import { TouchableOpacity } from "react-native";
 
 const Home = ({ navigation }) => {
   const [input, setInput] = useState("");
 
   return (
-    
-      <ScrollView style={styles.container}>
-        <ImageBackground
-          source={require("../assets/foto-fondo.png")}
-          style={styles.image}
-          resizeMode="cover"
-        />
-       
+    <>
+      <ImageBackground
+        source={require("../assets/foto-fondo.png")}
+        style={styles.image}
+      >
+        <ScrollView>
+          <View style={styles.container}>
+            <Image
+              source={require("../assets/logo-unlimited.png")}
+              style={styles.logo}
+            />
 
-                  <Image
-                    source={require("../assets/logo-unlimited.png")}
-                    style={styles.logo}
-                  />
+            <Text style={styles.titulo}> Únete ahora</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(input) => setInput(input)}
+              placeholder="Email"
+              name="email"
+              defaultValue={input}
+            />
 
-                  <Text style={styles.titulo}> Únete ahora</Text>
-                  <TextInput
-                    style={styles.input}
-                    onChangeText={(input) => setInput(input)}
-                    placeholder="Email"
-                    name="email"
-                    defaultValue={input}
-                    />
+            <TextInput
+              style={styles.input}
+              onChangeText={(input) => setInput(input)}
+              placeholder="Contraseña"
+              keyboardType="numeric"
+              name="contraseña"
+            />
+            <View style={styles.fixToText}>
+              <TouchableOpacity
+                style={buttonStyles.botonIngresar}
+                title="Unirse"
+                onPress={() =>
+                  navigation.navigate("CrearCuenta", { name: "CrearCuenta" })
+                }
+              >
+                <Text style={loginStyles.textoLogin}>Unirse</Text>
+              </TouchableOpacity>
 
-                 
-                  <TextInput
-                    style={styles.input}
-                    onChangeText={(input) => setInput(input)}
-                    placeholder="Contraseña"
-                    keyboardType="numeric"
-                    name="contraseña"
-                    />
-                  <View style={styles.fixToText}>
-                    <Button
-                      style={styles.button}
-                      title="Unirse"
-                      onPress={() =>
-                        navigation.navigate("CrearCuenta", { name: "CrearCuenta" })
-                      }
-                      accessibilityLabel="Crear una cuenta"
-                      />
-                    <Entypo name="circular-graph" size={15} color="#6D6A6A" />
-                  </View>
-                  <View style={styles.redireccionar}>
-                    <Text style={styles.textoInferior}>¿Ya estás en Unlimited?</Text>
-                    <Text
-                      onPress={() => navigation.navigate("Login", { name: "Login" })}
-                      style={styles.content}
-                      >
-                      Iniciar sesión
-                    </Text>
+              <Entypo
+                name="circular-graph"
+                size={15}
+                color="#6D6A6A"
+                style={styles.circle}
+              />
+            </View>
+            <View style={styles.redireccionar}>
+              <Text style={styles.textoInferior}>¿Ya estás en Unlimited?</Text>
+              <Text
+                onPress={() => navigation.navigate("Login", { name: "Login" })}
+                style={styles.content}
+              >
+                Iniciar sesión
+              </Text>
+            </View>
           </View>
-                    
-      </ScrollView>
-
+        </ScrollView>
+      </ImageBackground>
+    </>
   );
 };
 
